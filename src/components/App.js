6d909e15fa92291d24f2811a470857eb570721ae
Olga Nadongo <winnienadongo@gmail.com>
2022-07-29 15:17:15 +0300

    cleaning code

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,3 @@
-// import React, {useState, useEffect} from 'react';
-// import './App.css';
-// import Footer from './Footer';
-// import NavBar from './NavBar';
-// import NewContactForm from './NewContactForm';
-// import ContactContainer from './ContactContainer';
-
-
-// function App() {
-  
-//   const[contacts, setContacts]=useState([])
-   
-//   useEffect(()=>{
-//     fetch ("http://localhost:4000/contacts")
-//     .then(response=>response.json())
-//     .then(data=>setContacts(data))
-//     .catch(error=>console.log(error))
-//   },[])  
-
-//   function addAcontact(newContact){
-//     setContacts([...contacts,newContact])
-//   }
-
-//   return (
-//     <div className="App">
-//          <NavBar />
-//          <NewContactForm addAcontact={addAcontact}/>
-//          <ContactContainer contact={contacts} setContacts={setContacts}/>
-//          <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 
 import React,{useEffect, useState} from 'react';
 import './App.css';
@@ -70,9 +35,7 @@ function App() {
     <div>
           <NavBar />
         
-          {/* <NewContactForm addAcontact={addAcontact}/>
-          <ContactContainer contacts={contacts} setContacts={setContacts}  />
-          <Footer /> */}
+          
           <Routes>
               <Route path="/" element={ <><NewContactForm addAcontact={addAcontact}/>
           <ContactContainer contacts={contacts} setContacts={setContacts}  /></>} />
--- src/components/ContactContainer.js
+++ src/components/ContactContainer.js
@@ -1,54 +1,3 @@
-// import React,{useState} from 'react';
-// import Contact from './Contact'
-
-
-
-// function ContactContainer({contacts, setContacts}) {
-
-//   const [filter, setFilterItem]=useState('')
-   
-
-
-// //implementing delete
-// function deleteAcontact(id){
-//   setContacts(contacts.filter(contact=>contact.id !== id));
-//   }  
-
-// //implementing search
-// function  searchText(e){
-//    setFilterItem(e.target.value)
-// }
-
-// let  contactSearch=contacts.filter((contact)=>{
-// return Object.keys(contact).some(key=>
-//   contact[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
-  
-//   )
-// })
-
-
-// return (
-// <div className='contactscontainer'>
-
-//     <input type="text" className='searchContent' placeholder='Search...' value={filter}  onChange={searchText.bind(this)} />
-
-//   <div 
-//       className="contactbox"
-//       style={{
-//       textAlign: "center",
-//       marginLeft:"2" }}>
-    
-//   {
-//     contacts.map(contact=><Contact key={contact.id} id={contact.id} name={contact.name} email={contact.email} deleteAcontact={deleteAcontact} />
-// )}
-//   </div>
- 
-
- 
-// </div>)
-// }
-
-// export default ContactContainer;
 
 import React,{useState} from 'react';
 import Contact from './Contact';
@@ -87,7 +36,6 @@ function ContactContainer ({contacts,setContacts}) {
         <div 
             className="contactbox"
             style={{
-            // textAlign: "center",
             marginLeft:"2" }}>
           
         {
